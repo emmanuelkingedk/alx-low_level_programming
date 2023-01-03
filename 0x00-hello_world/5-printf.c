fd63ec78375a9796f36939b4664dff60cf887bdf
emmanuelkingedk <emmanuel.king350@gmail.com>
2023-01-02 20:49:18 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,6 @@
  * Return: 0 (success)
  */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n")
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
